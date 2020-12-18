9d9de9793a1dba1be60786948887d5d2ff2b8274
glu <vadimgirgenson@yandex.ru>
2020-12-19 02:19:43 +0300

    Added reversed function

--- vice/vice/urls.py
+++ vice/vice/urls.py
@@ -13,4 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-пg
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.main),
+    path('reverse/', views.to_reverse, name="rev"),
+]
--- vice/vice/views.py
+++ vice/vice/views.py
@@ -2,3 +2,11 @@ from django.shortcuts import render
 
 def main(request):
     return render(request, 'main_temp.html', {'greeting': 'Welcome'})
+
+
+def to_reverse(request):
+    request_get_free_input = request.GET['free_input']
+    reversed_text = request_get_free_input[::-1]
+    return render(request, 'reverse_temp.html', {'free_input': request_get_free_input,
+                                                 'reversed_input': reversed_text})
+
