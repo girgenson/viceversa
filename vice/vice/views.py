4131dbc84798ed3f88cdcb3740a29909886004eb
glu <vadimgirgenson@yandex.ru>
2020-12-19 03:12:01 +0300

    Count number of words

--- vice/vice/views.py
+++ vice/vice/views.py
@@ -5,8 +5,12 @@ def main(request):
 
 
 def to_reverse(request):
-    request_get_free_input = request.GET['free_input']
+    request_get_free_input = request.GET['free_input'].strip()
+    input_list = request_get_free_input.split(' ')
+    len_list = len(input_list)
     reversed_text = request_get_free_input[::-1]
     return render(request, 'reverse_temp.html', {'free_input': request_get_free_input,
-                                                 'reversed_input': reversed_text})
+                                                 'reversed_input': reversed_text,
+                                                 'list_len': len_list,
+                                                 'input_list': input_list})
 
